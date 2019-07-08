0f3810906e561428650cd3a0a2c8df8ea47b37b9
ErikGonzalez10 <erikgonzalezdelacruz@gmail.com>
2019-07-08 18:19:08 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,151 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Mi CV</title>
+ <script src="js/sweetalert.min.js"></script>
+    <link href="css/sweetalert.css" rel="stylesheet" type="text/css"/>
+
+	<link rel="stylesheet" href="css/estilos.css" >
+</head>
+<body>
+	<div class="contenedor">
+		<div class="Principal">
+			<div class="header">
+
+				<div>
+					
+					<img src=" imagen/imag.jpg" width="125" height="170">
+				</div>
+				 <script>
+       swal({   title: "Bienvenido Al CV de Eric Gonzalez", 
+             text: "se cerra en 5segundos",
+             timer: 5000,   
+             confirmButtonColor:	"#a90",
+             showConfirmButton: true });
+
+
+    </script>
+
+   
+
+
+				<div class="nombre">
+				<h1> Erik Gonzalez De La Cruz</h1>
+				<p><b>Direccion:</b>Álvaro Obregón #1, Localidad Pompeya, Veracruz 93680 Fijo:012263151045
+					<br>
+					<b>Celular:</b>2321354593 <b>Email:</b>erikgonzalezdelacruz@gmail.com
+				</p>
+
+
+
+			</div>
+			
+			</div>
+
+	</div>
+	
+
+
+
+
+
+	<div class="cuerpo">
+		<div id="contenido">
+
+		<h2 class="titulo">Resumen Profesional</h2>
+
+		<p>
+			Actualmente estoy trabajando para la compañía Voxpop como asesor técnico.
+La compañía opera desde hace 10 años en el país y mediante sus herramientas de publicidad busca crear un vínculo emocional entre la marca y el consumidor para que este tome la mejor decisión al momento de comprar.
+<br/>
+<br/>
+Deseo poner en practica todos los conocimientos adquiridos durante mi formación universitaria y poder formar parte de un equipo de trabajo que me permita desarrollarme personal y profesionalmente.
+
+		</p></div>
+
+
+
+		<div class="contenido">
+			<h2 class="titulo">Estudios</h2>
+			<p><b>"Universidad"</b><br/>
+				Instituto Tecnológico Superior Martínez De La Torre-Miguel Hidalgo No. 101, Col. Adolfo Ruiz
+Cortinez, Martínez De La Torre, Ver. 
+				<br/>
+				<br/>
+				<b>"Bachillerato</b>
+				<br/>
+				Tele bachillerato Independencia-Martínez Cartago 193 Villa Independencia, Martínez De La
+Torre, Ver.
+				<br/>
+				<br/>
+				<b>"Secundaria"</b>
+				<br/>
+				Telesecundaria Enrique C. Rebsamen-Principal Manuel Ávila Camacho S/N Loc. Pompeya, Atzalan, Ver.
+
+				<br/>
+				<br/>
+				<b>"Primaria"</b>
+				<br/>
+				Escuela Primaria Manuel Ávila Camacho-Principal Manuel Ávila Camacho S/N Loc. Pompeya, Atzalan, Ver.
+
+				<br/>
+				<br/>
+				<b>"Preescolar"</b>
+				<br/>
+				Jardín De Niños 21 De Marzo-Principal Manuel Ávila Camacho S/N Loc. Pompeya, Atzalan, Ver.
+
+			</p>
+		</div>
+
+		
+		<div class="contenido">
+			<h2 class="titulo">Hobbies</h2>
+			<p><b>Practicar Futbol Soccer </b> 
+				<p><b>Visitar Lugares. </b> 
+
+
+
+				
+
+
+
+			</b></p>
+		</div>
+
+<div class="contenido">
+			<h2 class="titulo">Experiencia Laboral</h2>
+			<b>Agricultor</b>,
+			Ocupacion:Trabajador de Campo....................(Agosto 2013-Febrero 2018)<br/><br/>
+
+			<b>Instructor del Área de Informática</b>,
+			Ocupacion:Profesor....................(mayo 2019-octubre 2019)<br/><br/>
+			<b>Asesor Técnico.</b>,
+			Ocupacion:ncargado....................(mayo 2019 actualidad)
+
+			 
+		</div>
+
+
+<div class="contenido">
+			<h2 class="titulo">referencia  Laboral</h2>
+			Ing.Jose
+			Celular:4344334343443
+			<br/><br/>
+			lic.marcos
+			celular:545545554 
+
+			 
+		</div>
+	</div>
+	
+
+
+
+
+
+	
+</body>
+</html>
+
